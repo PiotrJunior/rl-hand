b91342b85234a18b1cb5a804ac49f469ecf2bdc3
Piotr Maksymiuk <p.maksymiuk2@student.uw.edu.pl>
2026-05-29 15:32:01 +0200

    Init

---
+++ main.py
@@ -0,0 +1,53 @@
+import genesis as gs
+gs.init(backend=gs.cpu)
+
+scene = gs.Scene(
+    show_viewer=True,
+    viewer_options=gs.options.ViewerOptions(
+        camera_pos=(1, 1, 2),
+        camera_lookat=(0, 0, 1),
+        res=(1280, 720),
+        max_FPS=60,
+    ),
+)
+
+plane = scene.add_entity(gs.morphs.Plane())
+
+orca = scene.add_entity(
+    gs.morphs.URDF(
+        file='orcahand_description/v2/models/urdf/orcahand_right.urdf',
+        pos=(0.0, 0.0, 1.0),
+        fixed=True,
+    ),
+)
+
+scene.build()
+
+print("Liczba stopni swobody (DOFs):", orca.n_dofs)
+
+print("--- NAZWY DOF / STAWÓW ---")
+for i, joint in enumerate(orca.joints):
+    # Sprawdzamy czy dany staw ma stopnie swobody (nie jest zamrożony/stały)
+    print(f"Staw {i}: {joint.name} | Typ: {joint.type}")
+    # if joint.n_dofs > 0:
+        # print(f"DOF idx: {joint.dof_start_idx} | Staw: {joint.name} | Typ: {joint.type}")
+
+# 2. Alternatywnie: prosta lista samych nazw powiązanych bezpośrednio z indeksami DOF
+# print("\n--- KORTEŻ NAZW DOF ---")
+# dof_names = orca.dof_names
+# for idx, name in enumerate(dof_names):
+#     print(f"Indeks {idx}: {name}")
+
+# 2. Sterowanie pozycją stawów (Position Control)
+# Zakładamy, że robot ma np. 6 stawów. Podajemy pozycje w radianach.
+# target_positions = [0.0, 0.5, -0.5, 0.0, 1.0, 0.0]
+# orca.control_dofs_position(target_positions)
+
+# 3. Jeśli wolisz sterować prędkością (Velocity Control):
+# orca.control_dofs_velocity([0.1, 0.1, -0.1, 0.0, 0.0, 0.0])
+
+# 4. Jeśli chcesz "zresetować" stan robota natychmiastowo (bez fizyki):
+# orca.set_dofs_position([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+
+for i in range(1000):
+    scene.step()
